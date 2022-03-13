da52c00de01c67f0f4623c42f9b6601d6faf1892
SidDas8 <siddharthdas168@gmail.com>
2022-03-12 23:19:01 -0800

    Successfully appended Test Subject ID #s to dropdown menu.

--- static/app.js
+++ static/app.js
@@ -1,7 +1,7 @@
 // Function to initialize page
 function init() {
 
-    // Read data and save names
+    // Read data and save ID #s
     d3.json("../data/samples.json").then((data) => {
         var sample_names = data.names;
         console.log(sample_names)
@@ -9,14 +9,13 @@ function init() {
         // Reference the HTML dropdown select element
         var option_selector = d3.select("#selDataset");
 
-        // Append names to dropdown menu
-        
-    
+        // Append Test Subject ID #s to dropdown menu
+        sample_names.forEach((name) => {
+            option_selector.append("option").text(name).property("value", name);
+        });
 
+    });
 
+}
 
-
-
-    })
-  }
 init()
